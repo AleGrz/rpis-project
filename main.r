e1988b1e150e3c0b39b28f8cb5f85043875f798c
AleGrz <ajemalbatros@gmail.com>
2025-01-25 21:47:15 +0100

    i am malenia, blade of miquella

--- main.r
+++ main.r
@@ -18,6 +18,14 @@ prs <- function(f, point_count, bounds) {
   return(min_value)
 }
 
+compare_prs_ms <- function(f, bounds) {
+  print('MS')
+  ms_res <- replicate(70, ms(f, 100, bounds))
+  
+  print('PRS')
+  prs_res <- replicate(70, prs())
+  
+}
 
 get_alpine01_bounds <- function(dim) { 
   return(rep(c(0, 10), dim))
@@ -31,5 +39,6 @@ get_rosenbrock_bounds <- function(dim) {
 dimensions = c(2,10,20)
 
 for (dim in dimensions) {
-  
+  compare_prs_ms(makeRosenbrockFunction(dim), get_rosenbrock_bounds(dim))
+  compare_prs_ms(makeAlpine01Function(dim), get_alpine01_bounds(dim))
 }
